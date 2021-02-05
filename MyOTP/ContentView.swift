0e412287170269a2e79c3f569bbdf58d54a93cea
lex <alexander.koksharov@me.com>
2021-02-05 12:25:40 +0100

    Initial Commit

---
+++ MyOTP/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MyOTP
+//
+//  Created by Alexander Koksharov on 05.02.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
